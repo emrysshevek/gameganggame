cc83f6bde27d8a1a0b0b7ab79c7c21c880ef387b
Emrys Shevek <emrysshevek@gmail.com>
2026-02-07 22:25:53 -0800

    created value manager

---
+++ scenes/game/value_manager.gd
@@ -0,0 +1,48 @@
+class_name ValueManager
+extends Node
+
+#region Signals
+signal creature_value_increased(which_value)
+signal creature_value_decreased(which_value)
+signal creature_values_reduced()
+signal creature_values_cleared()
+#endregion
+
+
+enum CreatureValue {
+	ADAPTABILITY,
+	BRAVERY,
+	CURIOSITY,
+	DEPENDABILITY,
+	EMPATHY
+}
+
+#region Properties
+var creature_values: Array[CreatureValue] = [0,0,0,0,0]
+#endregion
+
+
+#region Methods
+func get_next_value(which_value: CreatureValue) -> CreatureValue:
+	return (which_value + 1) % len(creature_values)
+
+
+func increase_value(which_value: CreatureValue, amount: int) -> int:
+	creature_values[which_value] += amount
+	return creature_values[which_value]
+
+
+func decrease_value(which_value, amount) -> int:
+	creature_values[which_value] += amount
+	return creature_values[which_value]
+
+
+func reduce_values() -> void:
+	for i in len(creature_values):
+		creature_values[i] /= 2
+
+
+func clear_values() -> void:
+	for i in len(creature_values):
+		creature_values[i] = 0
+#endregion
