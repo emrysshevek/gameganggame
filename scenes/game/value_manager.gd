4621a031dd85056e7ebb94da0fee0295b7bc795f
Emrys Shevek <emrysshevek@gmail.com>
2026-02-07 22:43:57 -0800

    almost forget to emit signals

--- scenes/game/value_manager.gd
+++ scenes/game/value_manager.gd
@@ -2,8 +2,8 @@ class_name ValueManager
 extends Node
 
 #region Signals
-signal creature_value_increased(which_value)
-signal creature_value_decreased(which_value)
+signal creature_value_increased(which_value, amount)
+signal creature_value_decreased(which_value, amount)
 signal creature_values_reduced()
 signal creature_values_cleared()
 #endregion
@@ -36,20 +36,24 @@ func get_next_value(which_value: CreatureValue) -> CreatureValue:
 
 func increase_value(which_value: CreatureValue, amount: int) -> int:
 	creature_values[which_value] += amount
+	creature_value_increased.emit(creature_values[which_value], amount)
 	return creature_values[which_value]
 
 
-func decrease_value(which_value, amount) -> int:
+func decrease_value(which_value: CreatureValue, amount: int) -> int:
 	creature_values[which_value] += amount
+	creature_value_decreased.emit(which_value, amount)
 	return creature_values[which_value]
 
 
 func reduce_values() -> void:
 	for i in len(creature_values):
 		creature_values[i] /= 2
+	creature_values_reduced.emit()
 
 
 func clear_values() -> void:
 	for i in len(creature_values):
 		creature_values[i] = 0
+	creature_values_cleared.emit()
 #endregion
