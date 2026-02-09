ab29d4441f0c3c5bb224e3d9b680e32f2d275176
Emrys Shevek <emrysshevek@gmail.com>
2026-02-09 14:44:55 -0800

    pile positions cards

--- cards/pile.gd
+++ cards/pile.gd
@@ -16,26 +16,65 @@ var count: int:
 		return len(cards)
 @export var deck: Deck = null
 @export var is_faceup: bool = true
+
+@onready var ordered_cards: Array[Card] = cards
+#endregion
+
+
+#region Godot Built-in Methods
+func _ready() -> void:
+	_reposition_cards()
 #endregion
 
 
 #region Methods
 func shuffle() -> void:
-	push_warning("Function not implemented")
-	pass
+	ordered_cards.shuffle()
+	shuffled.emit()
 
 	
 func flip() -> void:
 	is_faceup = !is_faceup
-	pass
+	for card in cards:
+		if card.is_faceup != is_faceup:
+			card.flip()
 	
 
-func add_card(_card: Card) -> void:
-	push_warning("Function not implemented")
-	pass
+func add_card(_card: Card, _position:int=-1) -> void:
+	# cards are added to bottom of pile by default
+	# to insert to a specific index, set value of position
+	assert(_card not in cards)
+	cards.append(_card)
+	ordered_cards.insert(_position, _card)
+	card_added.emit(_card)
 
 
 func remove_card(_card: Card) -> void:
-	push_warning("Function not implemented")
-	pass
+	assert(_card in cards)
+	cards.erase(_card)
+	ordered_cards.erase(_card)
+	card_removed.emit()
 #endregion
+
+
+func _reposition_cards() -> void:
+	if count == 0:
+		return
+		
+	var area := size
+	var is_horizontal := size.x >= size.y
+	var spacing := Vector2(size.x / float(count + 1), 0)
+	var area_offset := Vector2(0, size.y / 2.0)
+	var card_offset := cards[0].size / 2.0
+	
+	if not is_horizontal:
+		area_offset = Vector2(size.x / 2.0, 0)
+		spacing = Vector2(0, size.y / float(count + 1))
+	if area == Vector2.ZERO:
+		spacing.y += 1
+	
+	var curr_pos := global_position + area_offset - card_offset
+	for card in cards:
+		curr_pos += spacing
+		card.global_position = curr_pos
+		
