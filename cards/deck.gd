c7ce544a41f4577f4702415b5e539e689e0bbc94
Emrys Shevek <emrysshevek@gmail.com>
2026-02-09 18:17:07 -0800

    save

--- cards/deck.gd
+++ cards/deck.gd
@@ -3,8 +3,8 @@ extends Node2D
 
 
 #region Signals
-signal card_added_to_deck(which_card: Card, which_deck: Deck)
-signal card_removed_from_deck(which_card: Card, which_deck: Deck)
+signal card_added(which_card: Card)
+signal card_removed(which_card: Card)
 #endregion
 
 
@@ -22,6 +22,7 @@ func add_card(which_card: Card) -> void:
 	push_warning("function not implented")
 	pass
 	
+
 func remove_card(which_card: Card) -> void:
 	push_warning("function not implemented")
 	pass
