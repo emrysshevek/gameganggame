0e00494af610386da61b7d6e893a742beda27d55
zimtallist <zimtallist@gmail.com>
2026-02-10 13:54:22 -0800

    Implemented Player
    Set up all the basic player functions and built out the ones I could for now. Added a test sprite for Player

---
+++ scenes/player.gd
@@ -0,0 +1,51 @@
+class_name player extends Node2D
+
+#region signals
+signal health_changed(which_player, old_value, new_value)
+signal move_requested(which_player, old_coord, new_coord)
+signal died(which_player)
+signal ended_turn(which_player)
+signal started_turn(which_player)
+#endregion
+
+#region properties
+var controller_id:int #will this be an int??
+var health_max:int = 5
+var health_current:int
+var deck:Deck
+var coordinates:Vector2
+#endregion
+
+#region methods
+func bind_controller(controller_info:int):
+	controller_id = controller_info
+
+func bind_deck(new_deck:Deck):
+	deck = new_deck
+
+func take_damage(amount:int):
+	health_current -= amount
+	health_changed.emit(self, health_current + amount, health_current)
+	if health_current <= 0:
+		die()
+		
+func heal(amount:int):
+	health_current += amount
+	if health_current > health_max:
+		health_current = health_max
+	
+func die():
+	died.emit(self)
+	
+func request_move(new_coordinates:Vector2):
+	move_requested.emit(self, coordinates, new_coordinates)
+	
+func complete_move(new_coordinates):
+	coordinates = new_coordinates
+	
+func end_turn():
+	ended_turn.emit(self)
+
+func start_turn():
+	started_turn.emit(self)
+#endregion
