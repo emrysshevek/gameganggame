5200080bb14520e0d44c7b195147e83f43b7e0cd
Emrys Shevek <emrysshevek@gmail.com>
2026-02-10 17:04:49 -0800

    small changes

--- entities/player.gd
+++ entities/player.gd
@@ -2,7 +2,7 @@ class_name player extends Node2D
 
 #region signals
 signal health_changed(which_player, old_value, new_value)
-signal move_requested(which_player, old_coord, new_coord)
+signal moved(which_player, old_coord, new_coord)
 signal died(which_player)
 signal ended_turn(which_player)
 signal started_turn(which_player)
@@ -37,11 +37,11 @@ func heal(amount:int):
 func die():
 	died.emit(self)
 	
-func request_move(new_coordinates:Vector2):
-	move_requested.emit(self, coordinates, new_coordinates)
-	
-func complete_move(new_coordinates):
+func move(new_coordinates):
+	var old_coords = coordinates
 	coordinates = new_coordinates
+	moved.emit(self, old_coords, new_coordinates)
+	
 	
 func end_turn():
 	ended_turn.emit(self)
