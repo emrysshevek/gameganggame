dadc5e0b283aa8e432d72f603ffde8321a0df1e0
zimtallist <zimtallist@gmail.com>
2026-02-21 21:45:42 -0800

    fixed minimap displaying incorrectly

--- scenes/origin_viewport.gd
+++ scenes/origin_viewport.gd
@@ -1,5 +1,7 @@
 class_name OriginViewport extends Node
 
+signal cursor_created(for_which_player)
+
 enum viewport_names{p1, p2, p3, p4, origin, minimap}
 
 #region properties
@@ -7,25 +9,18 @@ enum viewport_names{p1, p2, p3, p4, origin, minimap}
 @onready var player_viewport = preload("res://scenes/player_subviewport.tscn")
 @onready var grid_sprite = preload("res://entities/grid_sprite.tscn")
 @onready var cursor_sprite = preload("res://entities/cursor_sprite.tscn")
-var _player_sub_viewports:Dictionary[viewport_names, PlayerSubViewport]
 @onready var _world:World2D = $OriginViewportContainer/OriginViewport.find_world_2d()
 @onready var grid_man:GridManager = $OriginViewportContainer/OriginViewport/GridManager
 @onready var _origin_viewport:SubViewport = $OriginViewportContainer/OriginViewport
 @onready var _viewport_organizer_vertical:VBoxContainer = $VertViewportOrganizer
 @onready var _viewport_organizer_top_horizontal:HBoxContainer = $VertViewportOrganizer/TopHorViewportOrganizer
 @onready var _viewport_organizer_bottom_horizontal:HBoxContainer = $VertViewportOrganizer/BotHorViewportOrganizer
-@onready var _minimap_zoom:Vector2 = Vector2(0.2,0.2)
-@onready var _minimap_size:Vector2 = Vector2(200,200)
 var _player_view_zoom:Vector2 = Vector2(1.6,1.6)
-var _minimap_coord_1p:Vector2
-var _minimap_coord_2p:Vector2
-var _minimap_coord_4p:Vector2
 var _camera_limits:Dictionary[String,int]
 var _tile_size:Vector2
 var _player_viewport_size:Vector2 # = Vector2(100,100)#Vector2(DisplayServer.window_get_size().x / 2,DisplayServer.window_get_size().y / 2)
 @onready var player_to_character_sprite:Dictionary[Player, CharacterSprite]
 var player_cursors:Dictionary[int, Sprite2D]
-var number_of_players:int
 var character_sprites:Array[CharacterSprite]
 var _input_managers: Array[PlayerInputManager]
 @onready var player_viewport_names = [viewport_names.p1, viewport_names.p2, viewport_names.p3, viewport_names.p4]
@@ -42,30 +37,9 @@ func _ready() -> void:
 	_camera_limits["Right"] = (grid_man.map_width * _tile_size.x)
 	_camera_limits["Bottom"] = (grid_man.map_height * _tile_size.y)
 	#minimap placement and size configuration
-	_minimap_size = Vector2(((grid_man.map_width * _tile_size.x) * _minimap_zoom.x), ((grid_man.map_height * _tile_size.y) * _minimap_zoom.y))
-	_minimap_coord_1p = Vector2(DisplayServer.window_get_size().x - _minimap_size.x, 0)
-	_minimap_coord_2p = Vector2(DisplayServer.window_get_size().x - _minimap_size.x, (DisplayServer.window_get_size().y / 2) - _minimap_size.y/2)
-	_minimap_coord_4p = Vector2((DisplayServer.window_get_size().x / 2) - _minimap_size.x/2, (DisplayServer.window_get_size().y / 2) - _minimap_size.y/2)
-	setup_minimap()
 	#setup_player_testing()
 
-func setup_minimap():
-	number_of_players = 4 #testing for easy config of # of players
-	#setting up the minimap viewport
-	var minimap_viewport = player_viewport.instantiate()
-	add_child(minimap_viewport)
-	minimap_viewport.set_viewport_world(_world)
-	minimap_viewport.set_bounds(_minimap_size)
-	minimap_viewport.set_zoom(_minimap_zoom)
-	minimap_viewport.toggle_background()
-	minimap_viewport.set_fade(0.8)
-	var culling_dictionary:Dictionary[int,bool] = {0:true, 1:true}
-	minimap_viewport.set_layers_visible(culling_dictionary)
-	var minimap_camera_center_position = Vector2((grid_man.map_width * _tile_size.x) / 2, ( (grid_man.map_height * _tile_size.y)) / 2)
-	minimap_viewport.move_camera(minimap_camera_center_position)
-	_player_sub_viewports[viewport_names.minimap] = minimap_viewport
-
-func add_character() -> CharacterSprite:
+func add_character(character_id:int) -> CharacterSprite:
 	var new_character_sprite = character_sprite.instantiate()
 	new_character_sprite.set_sprite(load("res://art/test_cat.png"))
 	new_character_sprite.set_sprite_scale(Vector2(0.5,0.5))
@@ -73,6 +47,7 @@ func add_character() -> CharacterSprite:
 	_origin_viewport.add_child(new_character_sprite)
 	character_sprites.append(new_character_sprite)
 	_input_managers.append(new_character_sprite.input_man)
+	create_cursor(character_id, new_character_sprite.grid_coordinates)
 	
 	var grid_man_origin = grid_man.global_position
 	var test_player_coords:Array = [Vector2(1,1), Vector2(15,1), Vector2(6,14), Vector2(12,19)]
@@ -99,8 +74,10 @@ func create_cursor(which_player_id:int, tile_position:Vector2):
 	new_cursor.grid_coordinates = tile_position
 	new_cursor.set_visual_position(Vector2(character_sprites[0].grid_coordinates.x * _tile_size.x, character_sprites[0].grid_coordinates.y * _tile_size.y))
 	new_cursor.set_type(GridSprite.sprite_types.ui)
-	new_cursor.set_remote_camera_transform(_player_sub_viewports[player_viewport_names[which_player_id]].camera)
+	#the below switch of camera transform should happen when the PISM receives input to switch it to cursor mode
+	#new_cursor.set_remote_camera_transform(_player_sub_viewports[player_viewport_names[which_player_id]].camera)
 	new_cursor.move_request.connect(grid_man._on_grid_sprite_move_request)
+	new_cursor.visible = false
 	return new_cursor
 				
 #region testing methods
