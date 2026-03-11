0b28ff748dcca5518ef24ac33cf5c85fd78d8a74
Christie Partington <36772025+cpartington@users.noreply.github.com>
2026-03-10 18:16:56 -0700

    Initial ValueManager changes

--- global/config.gd
+++ global/config.gd
@@ -3,3 +3,5 @@ extends Node
 
 
 const MAX_PLAYER_COUNT = 4
+
+const VALUE_MANAGER_GROUP = 'value_manager'
--- scenes/value_manager.gd
+++ scenes/value_manager.gd
@@ -9,16 +9,8 @@ signal creature_values_cleared()
 #endregion
 
 
-enum CreatureValue {
-	ADAPTABILITY,
-	BRAVERY,
-	CURIOSITY,
-	DEPENDABILITY,
-	EMPATHY
-}
-
 #region Properties
-static var creature_values: Array[CreatureValue] = [0,0,0,0,0]
+static var creature_values: Array[Model.CreatureValue] = [0,0,0,0,0]
 static var value_names: Array[String] = [
 	"adaptability",
 	"bravery",
@@ -30,17 +22,21 @@ static var value_names: Array[String] = [
 
 
 #region Methods
-func get_next_value(which_value: CreatureValue) -> CreatureValue:
+func _ready() -> void:
+	add_to_group(Config.VALUE_MANAGER_GROUP)
+
+
+func get_next_value(which_value: Model.CreatureValue) -> Model.CreatureValue:
 	return (which_value + 1) % len(creature_values)
 
 
-func increase_value(which_value: CreatureValue, amount: int) -> int:
+func increase_value(which_value: Model.CreatureValue, amount: int) -> int:
 	creature_values[which_value] += amount
 	creature_value_increased.emit(creature_values[which_value], amount)
 	return creature_values[which_value]
 
 
-func decrease_value(which_value: CreatureValue, amount: int) -> int:
+func decrease_value(which_value: Model.CreatureValue, amount: int) -> int:
 	creature_values[which_value] += amount
 	creature_value_decreased.emit(which_value, amount)
 	return creature_values[which_value]
