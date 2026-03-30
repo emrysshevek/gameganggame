ff8b479dc3b3b751bf780b451623cb9a5987e172
zimtallist <zimtallist@gmail.com>
2026-03-30 13:18:39 -0700

    basic notifications with events global
    notifications for missing values, damage, looting cards and forced discard are set up to send to global notification view below minimap

---
+++ components/notification_log.gd
@@ -0,0 +1,34 @@
+extends Control
+
+@onready var labels:Array[Label] = [$TextLines/Label1, $TextLines/Label2, $TextLines/Label3, $TextLines/Label4, $TextLines/Label5]
+
+func _ready() -> void:
+	Events.character_damaged.connect(_on_character_damaged)
+	Events.forced_discard.connect(_on_forced_discard)
+	Events.looted_cards.connect(_on_looted_cards)
+	Events.missing_values.connect(_on_missing_values)
+
+func update(new_message_text:String):
+	labels[0].text = labels[1].text
+	labels[1].text = labels[2].text
+	labels[2].text = labels[3].text
+	labels[3].text = labels[4].text
+	labels[4].text = new_message_text
+	
+func _on_character_damaged(_character:Character, amount:int, source):
+	var source_name = "?"
+	if "type" in source:
+		source_name = Model.ObjectTypes.keys()[source.type]
+	update("P" + str(_character.character_id) + " took " + str(amount) + "dmg from " + source_name)
+
+func _on_forced_discard(_character:Character, source):
+	var source_name = "?"
+	if "type" in source:
+		source_name = Model.ObjectTypes.keys()[source.type]
+	update("P" + str(_character.character_id) + " force discard from " + source_name)
+	
+func _on_looted_cards(_character:Character):
+	update("P" + str(_character.character_id) + " looted cards")
+	
+func _on_missing_values(_card:Card):
+	update("P" + str(_card.owning_character.character_id) + " can't play card, missing values")
--- global/Events.gd
+++ global/Events.gd
@@ -3,6 +3,7 @@ extends Node
 #region Card
 signal card_played(_card: Card)
 signal card_discarded(_card: Card)
+signal missing_values(_card :Card)
 #endregion
 
 #region Deck
@@ -12,4 +13,10 @@ signal card_removed_from_deck(_card: Card, _deck: Deck)
 
 #region Input State Machine
 signal request_input_state_transition(new_state:String, requesting_character:Character)
+#endregion
+
+#region Character
+signal character_damaged(_character: Character, _amount:int, effect_source)
+signal looted_cards(_character: Character)
+signal forced_discard(_character: Character, effect_source)
 #endregion
