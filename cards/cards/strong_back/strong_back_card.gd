ab6f744404b345fd870bff44bf1b34ecb3bb0af0
zimtallist <zimtallist@gmail.com>
2026-04-23 20:12:53 -0700

    updated strong back to be more efficient per emrys

--- cards/cards/strong_back/strong_back_card.gd
+++ cards/cards/strong_back/strong_back_card.gd
@@ -3,13 +3,11 @@ extends Card
 ##discard all resource cards in hand and draw that many cards + 1
 
 func _trigger_play_ability() -> void:
-	var resource_cards:Array[Card]
+	var count:int = 0
 	var cards_in_hand:Array[Card] = owning_character.my_screen.card_manager.hand_pile.cards
 	for each_card in cards_in_hand:
 		if each_card is LootCard:
-			resource_cards.append(each_card)
-	var cards_to_draw:int = resource_cards.size() + 1
-	for each_card in resource_cards:
-		owning_character.my_screen.card_manager.discard(each_card)
-	owning_character.my_screen.card_manager.draw(cards_to_draw)
+			count += 1
+			owning_character.my_screen.card_manager.discard(each_card)
+	owning_character.my_screen.card_manager.draw(count + 1)
 	super._trigger_play_ability()
